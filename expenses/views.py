35c48cc8519e7ead9dd4b4270e1326472358aded
prativa thapa <prativat4959@gmail.com>
2026-06-11 18:37:46 +0545

    fix(views): import Sum and fix serialzer typo in expense_list POST response

--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Sum
 
 from .models import Category, Expense
 from .serializers import CategorySerializer, ExpenseSerializer
@@ -37,7 +38,7 @@ def expense_list(request):
     serializer = ExpenseSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
-    return Response(serialzer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
